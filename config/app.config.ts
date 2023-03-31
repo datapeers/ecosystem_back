a6606e2a66c3410c2bc447e6264baacd47fb9dc2
Jorge Fernandez <jorgejgdrj41@gmail.com>
2023-03-30 20:34:30 -0500

    Add graphql module

--- config/app.config.ts
+++ config/app.config.ts
@@ -1,6 +1,12 @@
 export const EnvConfiguration = () => ({
-  environment: process.env.NODE_ENV || "development",
+  environment: process.env.NODE_ENV || AppEnvironments.Development,
   port: process.env.PORT || 3000,
 });
 
 export type AppConfiguration = ReturnType<typeof EnvConfiguration>;
+
+export enum AppEnvironments {
+  Development = "development",
+  Staging = "staging",
+  Production = "production",
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { EnvConfiguration } from 'config/app.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppConfiguration, EnvConfiguration, AppEnvironments } from 'config/app.config';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -8,6 +11,21 @@ import { EnvConfiguration } from 'config/app.config';
       envFilePath: process.env.NODE_ENV ? `${process.cwd()}/env/${process.env.NODE_ENV}.env` : undefined,
       isGlobal: true,
       load: [ EnvConfiguration ]
+    }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      driver: ApolloDriver,
+      useFactory: async (configService: ConfigService<AppConfiguration>) => {
+        const enableDebug = configService.get('environment') === AppEnvironments.Development;
+        return {
+          cors: true,
+          debug: enableDebug,
+          playground: enableDebug,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+        }
+      }
     }),
   ],
   controllers: [],

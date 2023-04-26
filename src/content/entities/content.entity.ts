741ed9850ec2faa3dd41cfb1a09346ba09e16cee
JairoCarmonaB <jairo.carmona@datapeers.co>
2023-04-26 09:50:54 -0500

    working in content

--- src/content/entities/content.entity.ts
+++ src/content/entities/content.entity.ts
@@ -1,7 +1,53 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { SchemaTypes, Types, Document } from 'mongoose';
+import { Phase } from 'src/phases/entities/phase.entity';
 
+@Schema({ timestamps: true })
 @ObjectType()
 export class Content {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  _id: string;
+
+  // @Prop([{ type: SchemaTypes.ObjectId, ref: 'resources' }])
+  // recursos: string[];
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'content' }] })
+  childs: Content[];
+
+  // @Prop([
+  //   {
+  //     participantId: { type: SchemaTypes.ObjectId },
+  //     attendedBy: { type: SchemaTypes.ObjectId, ref: 'Empresario' },
+  //     date: { type: Date },
+  //   },
+  // ])
+  // attendance: {
+  //   participantId: string;
+  //   attendedBy: string;
+  //   date: Date;
+  // }[];
+
+  @Prop()
+  content: string;
+
+  @Prop({ default: false })
+  hide: boolean;
+
+  @Prop({ default: false })
+  deleted: boolean;
+
+  @Prop({ type: { type: Types.ObjectId, ref: 'phases' } })
+  phase: Phase;
+
+  @Prop({ type: Types. })
+  item: any;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
 }
+
+export const ContentSchema = SchemaFactory.createForClass(Content);

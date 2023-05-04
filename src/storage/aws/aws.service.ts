ed66abdd3f2cb6b99ac8874ccfd09c2ce1182003
JairoCarmonaB <jairo.carmona@datapeers.co>
2023-05-04 16:49:11 -0500

    added get url file

--- src/storage/aws/aws.service.ts
+++ src/storage/aws/aws.service.ts
@@ -1,33 +1,48 @@
 import { Injectable } from '@nestjs/common';
-import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
-import { getSignedUrl, } from "@aws-sdk/s3-request-presigner";
+import {
+  PutObjectCommand,
+  GetObjectCommand,
+  S3Client,
+  S3ClientConfig,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { StorageService } from '../models/storage-service';
 import { AppConfiguration } from 'config/app.config';
 import { ConfigService } from '@nestjs/config';
-
 
 @Injectable()
 export class AwsService implements StorageService {
   private readonly config: S3ClientConfig;
   private readonly defaultBucketName: string;
-  constructor(
-    private readonly configService: ConfigService<AppConfiguration>
-  ) {
-    const endpoint = this.configService.get("awsS3Uri");
-    this.defaultBucketName = this.configService.get("awsS3BucketName");
-    const region = this.configService.get("awsS3Region");
+  constructor(private readonly configService: ConfigService<AppConfiguration>) {
+    const endpoint = this.configService.get('awsS3Uri');
+    this.defaultBucketName = this.configService.get('awsS3BucketName');
+    const region = this.configService.get('awsS3Region');
     this.config = {
       region,
       endpoint,
       forcePathStyle: true,
+      credentials: {
+        accessKeyId: 'AKIA6DI6GSY2VKXUUAAF',
+        secretAccessKey: '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',
+      },
     };
   }
 
   async createPresignedUrl(key: string): Promise<string> {
     const client = new S3Client(this.config);
-    const config = { Bucket: this.defaultBucketName, Key: key }
+    const config = { Bucket: this.defaultBucketName, Key: key };
     const command = new PutObjectCommand(config);
-    return await getSignedUrl(client, command, { expiresIn: 3600 });
-  };
-};
+    return await getSignedUrl(client, command, { expiresIn: 60 * 3 });
+  }
 
+  async getPresignedUrl(key: string) {
+    const client = new S3Client(this.config);
+    const config = {
+      Bucket: this.defaultBucketName,
+      Key: key,
+    };
+    const command = new GetObjectCommand(config);
+    return await getSignedUrl(client, command, { expiresIn: 60 * 60 * 24 });
+  }
+}
